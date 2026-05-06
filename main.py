151bcdb542c8473d89f2c7253fc4c19399d52e89
WASHIMI <kokiwashi@gmail.com>
2026-05-07 00:03:36 +0900

    add requirements.txt

--- main.py
+++ main.py
@@ -88,3 +88,13 @@ def get_list():
         })
 
     return {"data": result}
+
+import os
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
