c80366d5d99a53bbd70ae9b2027fa25ab3e3f608
WASHIMI <kokiwashi@gmail.com>
2026-05-07 00:12:25 +0900

    add root endpoint

--- main.py
+++ main.py
@@ -98,3 +98,7 @@ app = FastAPI()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+@app.get("/")
+def root():
+    return {"message": "FastAPI is running!"}
